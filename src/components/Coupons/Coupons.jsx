14f2e100597e4ba0f38eb1310e9cb62fc8a6c00f
swapnilranmale2003 <swapnilranmale2003@gmail.com>
2024-04-02 21:59:49 +0530

    coupons component

--- src/components/Coupons/Coupons.jsx
+++ src/components/Coupons/Coupons.jsx
@@ -3,7 +3,51 @@ import "./Coupons.css";
 function Coupons() {
   return (
     <div>
-      <div className="container my-5">
+      <div className="container my-">
+        <div className="row">
+          <div className="col-md-4">
+            <div className="coupon">
+              <div className="title">
+                <h3>Name</h3>
+              </div>
+              <div className="description">
+                This is the coupon of course on Udemy
+              </div>
+              <div className="coupon-btn">
+                <button className="btn btn-dark">Link</button>
+                <button className="btn btn-dark">Redeem</button>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-4">
+          <div className="coupon">
+              <div className="title">
+                <h3>Name</h3>
+              </div>
+              <div className="description">
+                This is the coupon of course on Udemy
+              </div>
+              <div className="coupon-btn">
+                <button className="btn btn-dark">Link</button>
+                <button className="btn btn-dark">Redeem</button>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-4">
+          <div className="coupon">
+              <div className="title">
+                <h3>Name</h3>
+              </div>
+              <div className="description">
+                This is the coupon of course on Udemy
+              </div>
+              <div className="coupon-btn">
+                <button className="btn btn-dark">Link</button>
+                <button className="btn btn-dark">Redeem</button>
+              </div>
+            </div>
+          </div>
+        </div>
         <div className="row">
           <div className="col-md-4">
             <div className="coupon">
