c343a8bfb86ffe08dce73976cab8811e51951040
swapnilranmale2003 <swapnilranmale2003@gmail.com>
2024-04-02 19:10:30 +0530

    Frontpage

---
+++ src/Pages/FrontPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Navbar from "../components/Navbar/Navbar";
+import Coupons from "../components/Coupons/Coupons";
+
+function FrontPage() {
+  return <div>
+    <Navbar />
+    <Coupons />
+  </div>;
+}
+
+export default FrontPage;
