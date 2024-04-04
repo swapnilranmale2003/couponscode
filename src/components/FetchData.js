86c0eb90946cc599a557feffc960bee9f49dc38c
swapnilranmale2003 <swapnilranmale2003@gmail.com>
2024-04-05 00:51:28 +0530

    fetchdata is done

---
+++ src/components/FetchData.js
@@ -0,0 +1,51 @@
+import React, { useEffect, useState } from 'react';
+import './FetchData.css'; // Assuming you have a CSS file for styling
+
+function FetchData() {
+    const [records, setRecords] = useState([]);
+    
+    useEffect(() => {
+        fetch('https://uploadcoupons-6b1e4-default-rtdb.firebaseio.com/UploadCoupon.json')
+            .then(response => response.json())
+            .then(data => {
+                const fetchedData = [];
+                for (const key in data) {
+                    fetchedData.push({ id: key, ...data[key] });
+                }
+                setRecords(fetchedData);
+            })
+            .catch(err => console.log(err));
+    }, []);
+
+    const copyCouponCode = (code) => {
+        navigator.clipboard.writeText(code)
+            .then(() => alert('Coupon code copied to clipboard'))
+            .catch((error) => console.error('Failed to copy: ', error));
+    };
+
+    return (
+        <>
+        <div className="card-section ml-3 my-5">
+        <div className="cards-container">
+            {records.map((item, index) => (
+                <div className="card" key={index}>
+                    <div className="card-header">
+                        <h3>{item.title}</h3>
+                    </div>
+                    <div className="card-body">
+                        <p><strong>Coupon Code:</strong> {item.couponcode}</p>
+                        {item.description && <p><strong>Description:</strong> {item.description}</p>}
+                        <div className="buttons-container">
+                            {item.link && <button className="link-button"><a href={item.link} target="_blank" rel="noopener noreferrer">Link</a></button>}
+                            {item.couponcode && <button className="redeem-button" onClick={() => copyCouponCode(item.couponcode)}>Redeem</button>}
+                        </div>
+                    </div>
+                </div>
+            ))}
+        </div>
+        </div>
+        </>
+    );
+}
+
+export default FetchData;
