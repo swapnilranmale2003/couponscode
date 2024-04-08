4718476df2ffa5273bcff6d41b1457d5c4e9d94b
swapnilranmale2003 <swapnilranmale2003@gmail.com>
2024-04-08 10:41:38 +0530

    added css to Homepage

--- src/Pages/Homepage.jsx
+++ src/Pages/Homepage.jsx
@@ -1,17 +1,18 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./Homepage.css"; 
 function Homepage(props) {
   return (
-    <div>
-      <h1>
-        <Link to="/login">Login</Link>
-      </h1>
-      <br />
-      <h1>
-        <Link to="/signup">SignUp</Link>
-      </h1>
-      <br />
-      <h2> {props.name ? `Welcome - ${props.name}` : "Login Please"} </h2>
+    <div className="homepage-container">
+      <div className="box-container">
+        <div className="box">
+          <h1><Link to="/login" className="link-style">Login</Link></h1>
+        </div>
+        <div className="box">
+          <h1><Link to="/signup" className="link-style">SignUp</Link></h1>
+        </div>
+      </div>
+      <h2 className="welcome-message"> {props.name ? `Welcome - ${props.name}` : "Login Please"} </h2>
     </div>
   );
 }
