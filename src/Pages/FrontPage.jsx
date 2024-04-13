8ba89aedc6f1bf66b43318f05959089a039bcae1
swapnilranmale2003 <swapnilranmale2003@gmail.com>
2024-04-13 14:11:59 +0530

    homepage ready!

--- src/Pages/FrontPage.jsx
+++ src/Pages/FrontPage.jsx
@@ -2,25 +2,53 @@ import React from "react";
 import Navbar from "../components/Navbar/Navbar";
 import Footer from "../components/Footer/Footer";
 import { FaPlus } from "react-icons/fa6";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
 import FetchData from "../components/FetchData";
+import GetCoupons from "../components/Getcoupons/GetCoupons";
+import "./FrontPage.css";
 
 function FrontPage() {
   const navigate = useNavigate();
 
-  const handleUpload = () => {
-    navigate("/upload");
+  const handleBox1 = () => {
+    navigate("/categories/education");
+  };
+
+  const handleBox2 = () => {
+    navigate("/categories/food");
+  };
+
+  const handleBox3 = () => {
+    navigate("/categories/clothings");
   };
 
   return (
     <div>
       <Navbar />
-      {/* Your other content */}
-      {/* <div className="d-flex justify-content-center uploadicons container-fluid">
-        <FaPlus size={"5rem"} color="#05386b" onClick={handleUpload} />
-        <span className="align-self-center ml-2">Upload Coupon</span>
+      <div className="homepage-section">
+        <div className="hs-left">
+          <h1>Welcome</h1>
+          <img src="/c3.webp" alt="" />
+        </div>
+        <div className="hs-right">
+          <div className="cardFP">
+            <div className="row">
+              <div className="box1" onClick={handleBox1}>
+                <Link to={'/education'}>Educations</Link>
+              </div>
+              <div className="box1" onClick={handleBox2}>
+                <Link to={'/foods'}>Foods</Link>
+              </div>
+            </div>
+            <div className="row">
+              <div className="box1" onClick={handleBox3}>
+                <Link to={'/clothings'}>Clothings</Link>
+              </div>
+              <div className="box1">Others</div>
+            </div>
+          </div>
+        </div>
       </div>
-      <FetchData /> */}
       <Footer />
     </div>
   );
---
+++ src/components/Getcoupons/GetCoupons.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./GetCoupons.css";
+function GetCoupons() {
+  return (
+    <div className="get-section">
+      <div class="scene">
+        <div class="cube">
+          <span class="side top">Coupons</span>
+          <span class="side front">Get</span>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default GetCoupons;
